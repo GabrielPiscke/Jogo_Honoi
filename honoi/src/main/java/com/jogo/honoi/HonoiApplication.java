ec6da66746b8df4ec13e5efd694ce8ac5a1251d2
GabrielPiscke <gabrielmateuspiscke@gmail.com>
2025-06-17 14:11:18 -0300

    UpdSpring

---
+++ honoi/src/main/java/com/jogo/honoi/HonoiApplication.java
@@ -0,0 +1,13 @@
+package com.jogo.honoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonoiApplication.class, args);
+	}
+
+}
---
+++ honoi/src/test/java/com/jogo/honoi/HonoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogo.honoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
